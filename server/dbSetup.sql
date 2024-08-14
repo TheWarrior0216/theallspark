3ea12d856833c1d633188f496abe00cc3fac12dc
Seth Baxter <TheWarrior0216@gmail.com>
2024-08-14 15:32:21 -0600

    setup all connections and created most tables exept favorite which we were told we would struggle with

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -6,3 +6,26 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) UNIQUE COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8mb4 COMMENT '';
+
+
+CREATE TABLE recipe(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  title VARCHAR(255) NOT NULL ,
+  instruction VARCHAR(5000) NOT NULL,
+  img VARCHAR(1000) NOT NULL,
+  category ENUM('breakfast','lunch','dinner', 'snack', 'dessert') NOT NULL DEFAULT 'snack',
+  creatorId VARCHAR(255) NOT NULL,
+  FOREIGN KEY (creatorId) REFERENCES accounts (id) ON DELETE CASCADE
+)
+
+CREATE TABLE ingredient(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  name VARCHAR(255) NOT NULL,
+  quantity VARCHAR(255) NOT NULL,
+  recipeId INT NOT NULL,
+  Foreign Key (recipeId) REFERENCES recipe (id) ON DELETE CASCADE
+)
